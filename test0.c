41116bfeb2de7dc4fa933718b575a9bb6633e021
sonamu02 <smileon02@naver.com>
2018-02-06 23:42:24 +0900

    create project

---
+++ test0.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int TEST1(void)
+{
+ 	int i=0;
+	int x=0;
+	int y=0;
+	char string[]="test";
+	
+	return(x*y);
+
+}
---
+++ test1.c
@@ -0,0 +1,10 @@
+void TEST2(void)
+{
+
+	int a = 0;
+	int c = 1;
+	
+	int b = 0;
+	for(c=0;c<10;c++)
+	{}
+}
